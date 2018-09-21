61656fdcd7d87c342d7a3c07d4a4678dd99a81aa
chenyu-zheng <zcy9998@gmail.com>
2018-09-21 09:20:46 -0500

    Add 3 classes

---
+++ MVCAssignment1/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment1.Models
+{
+    public class Car
+    {
+        public decimal Cost { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ MVCAssignment1/Models/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment1.Models
+{
+    public class House
+    {
+        public decimal Value { get; set; }
+        public double LotSize { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
